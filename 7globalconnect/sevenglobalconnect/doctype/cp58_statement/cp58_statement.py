f14015a1f0449deedc9ab54421cd8a7cc63d9565
ashish-greycube <mr.ashish.shah@gmail.com>
2018-09-22 10:44:27 +0530

    address

--- 7globalconnect/sevenglobalconnect/doctype/cp58_statement/cp58_statement.py
+++ 7globalconnect/sevenglobalconnect/doctype/cp58_statement/cp58_statement.py
@@ -6,7 +6,9 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe.contacts.address_and_contact import load_address_and_contact
+import frappe.defaults
 
 class CP58Statement(Document):
 	def onload(self):
+		self.company=frappe.db.get_single_value('Global Defaults', 'default_company')
 		load_address_and_contact(self, "company")
